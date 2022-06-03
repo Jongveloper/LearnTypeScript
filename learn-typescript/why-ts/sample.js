91430c8aff11940bf08daee0f9c536c4b6f7b221
Woo Dy <kjop7419@naver.com>
2022-06-03 20:07:44 +0900

    :memo: 타입스크립트 프로젝트를 시작하는 법(인프런 캡틴판교님)

--- learn-typescript/why-ts/sample.js
+++ learn-typescript/why-ts/sample.js
@@ -1,15 +1,15 @@
+// // function sum(a, b) {
+// //   return a + b;
+// // }
+// // sum(10, '20'); // 1020
+
+// // @ts-check
+// /**
+//  *
+//  * @param {number} a 첫번째 숫자
+//  * @param {number} b 두번째 숫자
+//  */
+
 // function sum(a, b) {
 //   return a + b;
 // }
-// sum(10, '20'); // 1020
-
-// @ts-check
-/**
- *
- * @param {number} a 첫번째 숫자
- * @param {number} b 두번째 숫자
- */
-
-function sum(a, b) {
-  return a + b;
-}
