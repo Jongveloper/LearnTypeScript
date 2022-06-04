0cd7a1898c2ff689e7af055ec6fed47deefa881c
Woo Dy <kjop7419@naver.com>
2022-06-04 18:57:45 +0900

    :memo: 타입 단언(인프런 캡틴판교님)

--- learn-typescript/class-note/9_type-inference.ts
+++ learn-typescript/class-note/9_type-inference.ts
@@ -1,38 +1,40 @@
-// 타입 추론 기본 1
-let a = ['abc'];
+{
+  // 타입 추론 기본 1
+  let a = ['abc'];
 
-function getB(b = 10) {
-  let c = 'hi';
-  return b + c;
+  function getB(b = 10) {
+    let c = 'hi';
+    return b + c;
+  }
+
+  // 타입 추론 기본 2
+  // interface Dropdown<T> {
+  //   value: T;
+  //   title: string;
+  // }
+
+  // let shoppingItem: Dropdown<string> = {
+  //   value: 'dd',
+  //   title: 'dd',
+  // };
+
+  // 타입 추론 기본 3
+  interface Dropdown<T> {
+    value: T;
+    title: string;
+  }
+  interface DetailedDropdown<K> extends Dropdown<K> {
+    description: string;
+    tag: K;
+  }
+
+  let detailedItem: DetailedDropdown<string> = {
+    title: 'abc',
+    description: 'ab',
+    value: 'cde',
+    tag: 'a',
+  };
+
+  // Best Common Type
+  let arr = [1, 2, true, true, 'a'];
 }
-
-// 타입 추론 기본 2
-// interface Dropdown<T> {
-//   value: T;
-//   title: string;
-// }
-
-// let shoppingItem: Dropdown<string> = {
-//   value: 'dd',
-//   title: 'dd',
-// };
-
-// 타입 추론 기본 3
-interface Dropdown<T> {
-  value: T;
-  title: string;
-}
-interface DetailedDropdown<K> extends Dropdown<K> {
-  description: string;
-  tag: K;
-}
-
-let detailedItem: DetailedDropdown<string> = {
-  title: 'abc',
-  description: 'ab',
-  value: 'cde',
-  tag: 'a',
-};
-
-// Best Common Type
-let arr = [1, 2, true, true, 'a'];
