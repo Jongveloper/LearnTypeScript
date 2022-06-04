bd6a64a0fe12a3e4cb7b16efe6c1e022245d5ff6
Woo Dy <kjop7419@naver.com>
2022-06-04 17:44:16 +0900

    :memo: 타입 추론(인프런 캡틴판교님)

---
+++ learn-typescript/class-note/9_type-inference.ts
@@ -0,0 +1,38 @@
+// 타입 추론 기본 1
+let a = ['abc'];
+
+function getB(b = 10) {
+  let c = 'hi';
+  return b + c;
+}
+
+// 타입 추론 기본 2
+// interface Dropdown<T> {
+//   value: T;
+//   title: string;
+// }
+
+// let shoppingItem: Dropdown<string> = {
+//   value: 'dd',
+//   title: 'dd',
+// };
+
+// 타입 추론 기본 3
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+interface DetailedDropdown<K> extends Dropdown<K> {
+  description: string;
+  tag: K;
+}
+
+let detailedItem: DetailedDropdown<string> = {
+  title: 'abc',
+  description: 'ab',
+  value: 'cde',
+  tag: 'a',
+};
+
+// Best Common Type
+let arr = [1, 2, true, true, 'a'];
